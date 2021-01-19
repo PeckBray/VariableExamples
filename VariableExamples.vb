bcadb0b448c0de196519a76495af5f220a49d858
Brayden Peck <brayden.peck2@gmail.com>
2021-01-19 08:50:42 -0700

    Add comments and wrap lines

--- VariableExamples.vb
+++ VariableExamples.vb
@@ -6,27 +6,31 @@
 
 Option Strict On    'No conversions by compiler
 Option Explicit On  'Must declare all variables
-Module Module1
+Module Module1 'PascalCase
 
     Sub Main()
         Dim x As Integer
         Dim y As Integer
-        Dim personName As String
         Dim result As Integer
+
+        Dim personName As String    'camelCase
         Dim firstName As String
         Dim lastName As String
         Dim age As Integer
 
         x = 1
         y = 2
-        result = x + y
+        result = x + CInt(y)    'Force to convert to integer
 
         firstName = "Joe"
         lastName = "Barnes"
         age = 42
 
         personName = firstName & " " & lastName
-        Console.WriteLine(personName & " is " & CStr(age) & " years old")
+        Console.WriteLine(personName _
+                            & " is " _
+                         & CStr(age) _
+                     & " years old") 'Wrap long lines with Space and "_"
         Console.Read()
     End Sub
 
